bfeef047cf2b4447b460a84e235714885b33cf34
monju1125 <monju1125@github.com>
2026-05-30 02:07:22 +0000

    Test run: 3/9 passed, 6 failed, 0 skipped - 2026-05-30T02:07:22.745Z

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -66,8 +66,10 @@ public class SampleClass {
     //    implement a custom .equals(Object other){} method here.
     @Override
     public boolean equals(Object other){
-        if (b == true) return b;
-          else return !b;
+        if ( this == (SampleClass) other) {
+            return true;
+        }
+        else return false;
     }
         @Override
         public String toString(){
