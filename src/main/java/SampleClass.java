1850b393c2fe98368bf975f45576e3eba2a55bd2
monju1125 <monju1125@github.com>
2026-05-30 02:38:11 +0000

    Test run: 7/9 passed, 2 failed, 0 skipped - 2026-05-30T02:38:11.539Z

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -67,6 +67,8 @@ public class SampleClass {
     @Override
     public boolean equals(Object other){
         if (this == other) return true;
+        if ((other == null || getClass() != other.getClass()) ) return false;
+            
         SampleClass obj = (SampleClass) other;
         return false;
     }
